ce785d512adb2d9ac63dbb554c9357250c6d2a6e
SebastianBarros <sebastian.barros@hotmail.com>
2023-05-09 10:23:43 -0300

    first commit

---
+++ systemMessage.parser.js
@@ -0,0 +1,13 @@
+import fs from "fs/promises";
+import text from "./messages.js";
+
+const parsedMessages = text.split("\n").reduce((acum, curr) => {
+  const key = curr.split("=")[0].trim();
+  const value = curr.split("//")[1]?.trim();
+  return {
+    ...acum,
+    [key]: value,
+  };
+}, {});
+
+fs.writeFile("./system-messages.json", JSON.stringify(parsedMessages));
