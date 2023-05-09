05f1df707362b6930feeb054b58de6bfa50d2cc3
SebastianBarros <sebastian.barros@hotmail.com>
2023-05-09 10:46:51 -0300

    added default value

--- systemMessage.parser.js
+++ systemMessage.parser.js
@@ -3,7 +3,7 @@ import text from "./messages.js";
 
 const parsedMessages = text.split("\n").reduce((acum, curr) => {
   const key = curr.split("=")[0].trim();
-  const value = curr.split("//")[1]?.trim();
+  const value = curr.split("//")[1]?.trim() || '';
   return {
     ...acum,
     [key]: value,
